266e8859c781d427fba64ad850b365be4462345e
106529 <106529@WLK208ZM>
2021-12-16 14:48:40 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,177 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using PhoneMax_1._1.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhoneMax_1._1.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public IConfiguration Configuration { get; }
+        public HomeController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult UserDetails()
+        {
+            return View();
+        }
+        public IActionResult LoginSignupPage()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult LoginSignupPage(Registration registration)
+        {
+            Registration register = new Registration();
+            string connectionString = Configuration["ConnectionStrings:MyConnection"];
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string sql = $"Insert Into Register (Name, Email, Password) Values ('{registration.Name}', '{registration.Email}','{registration.Password}')";
+
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    command.CommandType = CommandType.Text;
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
+
+
+            ViewBag.Result = 1;
+       
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                DataTable dataTable = new DataTable();
+                string sql1 = $"Select * From Register";
+                SqlCommand command = new SqlCommand(sql1, connection);
+
+                connection.Open();
+
+                using (SqlDataReader dataReader = command.ExecuteReader())
+                {
+                    while (dataReader.Read())
+                    {
+                        register.LoginEmail = Convert.ToString(dataReader["email"]);
+                        register.LoginPassword = Convert.ToString(dataReader["password"]);
+                    }
+                }
+
+                connection.Close();
+            }
+            return View();
+        }
+            public IActionResult Android()
+        {
+            return View();
+        }
+        public IActionResult Galaxy_M52()
+        {
+            return View();
+        }
+        public IActionResult GalaxyZ()
+        {
+            return View();
+        }
+        public IActionResult GalaxyNote()
+        {
+            return View();
+        }
+        public IActionResult GalaxyS21()
+        {
+            return View();
+        }
+        public IActionResult Redmi10Prime()
+        {
+            return View();
+        }
+        public IActionResult Redmi9()
+        {
+            return View();
+        }
+        public IActionResult RedmiNote10S()
+        {
+            return View();
+        }
+        public IActionResult Redmi9Activ()
+        {
+            return View();
+        }
+        public IActionResult A74()
+        {
+            return View();
+        }
+        public IActionResult A31()
+        {
+            return View();
+        }
+        public IActionResult A16()
+        {
+            return View();
+        }
+        public IActionResult F19()
+        {
+            return View();
+        }
+        public IActionResult Iphone()
+        {
+            return View();
+        }
+        public IActionResult Iphone5()
+        {
+            return View();
+        }
+        public IActionResult Iphone6()
+        {
+            return View();
+        }
+        public IActionResult Iphone7()
+        {
+            return View();
+        }
+        public IActionResult Iphone8()
+        {
+            return View();
+        }
+        public IActionResult Iphone11Pro()
+        {
+            return View();
+        }
+        public IActionResult Iphone11ProMax()
+        {
+            return View();
+        }
+        public IActionResult Iphone12Pro()
+        {
+            return View();
+        }
+        public IActionResult Iphone13ProMax()
+        {
+            return View();
+        }
+        public IActionResult Windows()
+        {
+            return View();
+        }
+        public IActionResult Sucess()
+        {
+            return View();
+        }
+        public IActionResult Cart()
+        {
+            return View();
+        }
+    }
+}
